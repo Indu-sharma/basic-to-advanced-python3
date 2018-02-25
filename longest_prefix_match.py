54db96ec164079784de2fc9945f55b89d168ab65
Indu Sharma <indu.sharma.acharya@gmail.com>
2018-02-25 16:37:07 +0530

    Update longest_prefix_match.py

--- longest_prefix_match.py
+++ longest_prefix_match.py
@@ -1,3 +1,9 @@
+'''
+Longest network prefix matching program using Python This utility is useful when one has to find the longest matching prefix for the list of IP address. Pre-requisite for this utility: download and python import module SubnetTree
+
+Usage: longest_prefix_match.py {Input file with list of Prefixes} {Input file with list of IP addresses}
+'''
+
 import sys
 import re
 import SubnetTree
