637560c838809150bb791bc0ba119fbf4f509870
Ishar <indu.sharma.acharya@gmail.com>
2018-03-11 22:07:18 +0530

    Script for Internet forwarding in the ubuntu
    Script to configure Internet forwarding in the Ubuntu server

---
+++ inertnet_forward_linux.sh
@@ -0,0 +1,14 @@
+if [ $# -ne 3 ] ; then
+	echo "Usage : `basename $0` <private nework> <Internet facing interface name> <Private networking facing interface>"
+        echo "Example: `basename $0` '10.2.0.0/16' eth1 eth0"
+	exit
+fi
+private_net=$1
+internet=$2
+private=$3  
+echo 1 > /proc/sys/net/ipv4/ip_forward
+sudo iptables -A FORWARD -o $internet -i $private -m conntrack --ctstate -NEW -j ACCEPT
+sudo iptables -t nat -F POSTROUTING
+sudo iptables -t nat -A POSTROUTING -s $private_net -o $internet -j MASQUERADE 
+sudo iptables-save | sudo tee /etc/iptables.sav
+sudo iptables-restore < /etc/iptables.sav
