1753c6bc14211cf093cc87bf639a708ddbe37572
Ishar <indu.sharma.acharya@gmail.com>
2018-04-04 02:12:19 +0530

    Create SetUp_TearDown_Pyspark.py

---
+++ Python_generic/SetUp_TearDown_Pyspark.py
@@ -0,0 +1,13 @@
+import pytest
+import elasticsearch as ES
+
+@pytest.fixture(scope="module")
+def myconn(es="192.168.1.1:9200"):
+    my_es = ES(es)
+    yield my_es
+    my_es.close()
+
+@pytest.mark.elastic
+def test_getindex(myconn):
+    index = myconn.func_name
+    assert index == "xyz"
