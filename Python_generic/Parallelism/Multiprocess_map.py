d3a13b019d43f8b16873e817dca2f59ce162311d
Ishar <indu.sharma.acharya@gmail.com>
2018-04-19 20:52:49 +0530

    Create Multiprocess_map.py

---
+++ Python_generic/Parallelism/Multiprocess_map.py
@@ -0,0 +1,20 @@
+import multiprocessing
+import time
+
+def usingmap_pool(mylist):
+    mysum = 0
+    for i in mylist:
+        mysum = mysum + reduce(lambda x,y : x+y,range(1000))
+
+    return mysum
+
+
+
+if __name__=='__main__':
+    p = multiprocessing.Pool(processes = 100)
+    result = p.map(usingmap_pool, range(1,100))
+    p.start()
+    p.close()
+    p.join()
+
+    print(result)
