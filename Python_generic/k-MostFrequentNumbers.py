556d69b82ca6a4dffe68971da7428d8b30a0f4f0
Ishar <indu.sharma.acharya@gmail.com>
2021-02-24 15:20:09 +0530

    Update k-MostFrequentNumbers.py

--- Python_generic/k-MostFrequentNumbers.py
+++ Python_generic/k-MostFrequentNumbers.py
@@ -1,3 +1,10 @@
+# Problem Statement : Given the following List of Integers and value of 'k', find the most common 'k' values in the list. 
+input_arr = [1, 3, 4, 5, 6, 7, 8, 4, 4, 8, 10, 20, 17, 15, 1, 3, 5]
+k = 5
+
+
+# Solution - 1 : Using Naive Approach 
+
 def k_most_frequent(k, input_arr):
     """ Given an Array find the Most K-frequent Numbers"""
     results = []
@@ -32,11 +39,22 @@ def k_most_frequent(k, input_arr):
     return results
 
 
-# Testing/Driver code
-
-input_arr = [1, 3, 4, 5, 6, 7, 8, 4, 4, 8, 10, 20, 17, 15, 1, 3, 5]
-k = 5
-
 print(k_most_frequent(k, input_arr))
+
+# Solution - 2 : Using Python Dictionary Approach 
+
+from collections import defaultdict
+
+my_dict=defaultdict(int)
+for num in input_arr:
+    my_dict[num] += 1
+my_dict = dict(sorted(my_dict.items(),reverse=True, key= lambda x: x[1]))
+print(list(my_dict.keys())[:k] )
+
+# Solution - 3 : Using Python Counter module from collection 
+
+from collections import Counter
+Counter = Counter(input_arr)
+print(list(dict(Counter.most_common(k)).keys()))
 
 # O/p : [4, 1, 3, 5, 8]
