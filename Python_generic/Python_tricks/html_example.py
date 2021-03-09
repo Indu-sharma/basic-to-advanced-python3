b72696f3fb6849156fa58f3ee71bbcf60f91396f
Indu Sharma <13970131+Indu-sharma@users.noreply.github.com>
2021-03-10 01:45:51 +0530

    Create html_example.py

---
+++ Python_generic/Python_tricks/html_example.py
@@ -0,0 +1,62 @@
+from html.parser import HTMLParser
+
+
+class MyHTMLParser(HTMLParser):
+    def handle_starttag(self, tag, attrs):
+        print(f'Start : {tag}')
+        for atr in attrs:
+            print(f'-> {atr[0]} > {atr[1]}')
+    def handle_endtag(self, tag):
+        print(f'End   : {tag}')
+    def handle_startendtag(self, tag, attrs):
+        print(f'Empty : {tag}')
+        for atr in attrs:
+            print(f'-> {atr[0]} > {atr[1]}')
+        
+    def handle_comment(self, data):
+        pass
+
+if __name__ == '__main__':
+    n = int(input())
+    s = ''
+    for i in range(n):
+        s += input()       
+    parser = MyHTMLParser()
+    parser.feed(s)
+
+"""
+Input 
+2
+<html><head><title>HTML Parser - I</title></head>
+<body data-modal-target class='1'><h1>HackerRank</h1><br /></body></html>
+Your Output (stdout)
+Start : html
+Start : head
+Start : title
+End   : title
+End   : head
+Start : body
+-> data-modal-target > None
+-> class > 1
+Start : h1
+End   : h1
+Empty : br
+End   : body
+End   : html
+Expected Output
+
+Start : html
+Start : head
+Start : title
+End   : title
+End   : head
+Start : body
+-> data-modal-target > None
+-> class > 1
+Start : h1
+End   : h1
+Empty : br
+End   : body
+End   : html
+
+"""
