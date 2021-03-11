8a5c968f6d37c2aed839dc44fb3237ea7dac8b9a
Indu Sharma <13970131+Indu-sharma@users.noreply.github.com>
2021-03-11 06:30:30 +0530

    Update xml_example.py

--- Python_generic/Python_tricks/xml_example.py
+++ Python_generic/Python_tricks/xml_example.py
@@ -1,6 +1,10 @@
 import sys
 import xml.etree.ElementTree as etree
+
+
 """
+Example -1:
+
 You are given a valid XML document, and you have to print its score. The score is calculated by the sum of the score of each element. For any element, the score is equal to the number of attributes it has.
 The feed and subtitle tag have one attribute each - lang.
 The title and updated tags have no attributes.
@@ -28,3 +32,33 @@ if __name__ == '__main__':
     tree = etree.ElementTree(etree.fromstring(xml))
     root = tree.getroot()
     print(get_attr_number(root))
+    
+    
+"""
+Example - 2:
+
+You are given a valid XML document, and you have to print the maximum level of nesting in it. Take the depth of the root as .
+
+"""
+
+import xml.etree.ElementTree as etree
+
+
+maxdepth = 0
+def depth(node, level): 
+    global maxdepth
+    if (level == maxdepth):
+        maxdepth += 1
+    for child in node:
+        depth(child, level + 1) 
+
+if __name__ == '__main__':
+    n = int(input())
+    xml = ""
+    for i in range(n):
+        xml =  xml + input() + "\n"
+    tree = etree.ElementTree(etree.fromstring(xml))
+    depth(tree.getroot(), -1)
+    print(maxdepth)    
+
+    
