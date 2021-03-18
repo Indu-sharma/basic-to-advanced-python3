45f12f35ff331c041f9319791bc986f9e16718d5
Indu Sharma <13970131+Indu-sharma@users.noreply.github.com>
2021-03-19 00:59:36 +0530

    Update single_linkedlist.py

--- python data structure/linkedlist/single_linkedlist.py
+++ python data structure/linkedlist/single_linkedlist.py
@@ -40,8 +40,7 @@ class SingleLinkedList:
             i = i.next
 
     def __str__(self):
-        self.display()
-        return str(self.my_list)
+        return ','.join([str(i) for i in self])
 
 
 if __name__ == '__main__':
