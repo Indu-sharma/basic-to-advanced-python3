af6fe630a6451c9fa46d7b365d1594f703ea4b9e
Indu Sharma <13970131+Indu-sharma@users.noreply.github.com>
2021-03-18 23:31:06 +0530

    Update single_linkedlist.py

--- python data structure/linkedlist/single_linkedlist.py
+++ python data structure/linkedlist/single_linkedlist.py
@@ -8,6 +8,7 @@ class SingleLinkedList:
     def __init__(self):
         self.head = None
         self.tail = None
+        self.my_list = []
 
     def insert_start(self, value):
         new_node = Node(value)
@@ -23,12 +24,24 @@ class SingleLinkedList:
         i.next = new_node
         new_node.next = self.tail
 
+    def __iter__(self):
+        """
+        This makes LinkedList an Iterable.
+        """
+        i = self.head
+        while i is not None:
+            yield i.value
+            i = i.next
+
     def display(self):
         i = self.head
-
-        while i != None:
-            print(i.value)
+        while i is not None:
+            self.my_list.append(i.value)
             i = i.next
+
+    def __str__(self):
+        self.display()
+        return str(self.my_list)
 
 
 if __name__ == '__main__':
@@ -38,9 +51,11 @@ if __name__ == '__main__':
     single_list.insert_start(20)
     single_list.insert_start(30)
     print('Linkedlist after inserting 10,20,30 at the beginning of LinkedList')
-    single_list.display()
+    print(list(single_list))
     single_list.insert_end(100)
     single_list.insert_end(200)
     single_list.insert_end(300)
     print('Linkedlist after inserting 100,200,300 at the end of LinkedList')
-    single_list.display()
+    print(list(single_list))
+    print(f'Display LinkedList as list :: __str__ implementation is called')
+    print(single_list)
