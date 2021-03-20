4f4475bae3a768cb2d017b9c3913aa61f8948f95
Indu Sharma <13970131+Indu-sharma@users.noreply.github.com>
2021-03-20 19:48:09 +0530

    Update single_linkedlist.py

--- python data structure/linkedlist/single_linkedlist.py
+++ python data structure/linkedlist/single_linkedlist.py
@@ -8,21 +8,30 @@ class SingleLinkedList:
     def __init__(self):
         self.head = None
         self.tail = None
-        self.my_list = []
+
+    def createSl(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
 
     def insert_start(self, value):
-        new_node = Node(value)
-        new_node.next = self.head
-        self.head = new_node
+        if not self.head:
+            self.createSl(value)
+        else:
+            new_node = Node(value)
+            new_node.next = self.head
+            self.head = new_node
 
     def insert_end(self, value):
-        i = self.head
-
-        while i.next != self.tail:
-            i = i.next
-        new_node = Node(value)
-        i.next = new_node
-        new_node.next = self.tail
+        if not self.head:
+            self.createSl(value)
+        else:
+            i = self.head
+            while i.next is not None:
+                i = i.next
+            new_node = Node(value)
+            i.next = new_node
+            new_node.next = None
 
     def __iter__(self):
         """
@@ -31,12 +40,6 @@ class SingleLinkedList:
         i = self.head
         while i is not None:
             yield i.value
-            i = i.next
-
-    def display(self):
-        i = self.head
-        while i is not None:
-            self.my_list.append(i.value)
             i = i.next
 
     def __str__(self):
@@ -50,11 +53,9 @@ if __name__ == '__main__':
     single_list.insert_start(20)
     single_list.insert_start(30)
     print('Linkedlist after inserting 10,20,30 at the beginning of LinkedList')
-    print(list(single_list))
+    print(single_list)
     single_list.insert_end(100)
     single_list.insert_end(200)
     single_list.insert_end(300)
     print('Linkedlist after inserting 100,200,300 at the end of LinkedList')
-    print(list(single_list))
-    print(f'Display LinkedList as list :: __str__ implementation is called')
     print(single_list)
