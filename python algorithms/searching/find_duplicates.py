201d4de8f093a98b0d4d88b63545f6413af663a6
Indu Sharma <13970131+Indu-sharma@users.noreply.github.com>
2021-03-21 14:01:13 +0530

    Update find_duplicates.py

--- python algorithms/searching/find_duplicates.py
+++ python algorithms/searching/find_duplicates.py
@@ -1,10 +1,25 @@
-# Problem : Find the duplicate elements/check duplicity in the list. 
+# Problem : Find the duplicate elements or check duplicity in the array.
 
 """
-Solution-1: Keep track of visited list.
-Time Complexity: O(n^2), Space Complexity : O(n)
+Solution-1: Naive solution using nested loop.
+Time Complexity: O(n^2) , Space Complexity : O(n)
+
+"""
+print('*******Solution - 1*********')
+my_list = [5, 4, 1, 2, 3, 5]
+print('Duplicate Elements :')
+for i in range(len(my_list)):
+    for j in range(i+1,len(my_list)):
+        if my_list[i] == my_list[j]:
+            print(my_list[i])
+
+
+"""
+Solution-2: Keep track of visited list.
+Time Complexity: Better than O(n^2) in most cases, Space Complexity : O(n)
 """
 
+print('**********Solution - 2**********')
 my_list = [5, 4, 1, 2, 3, 5]
 visited = []
 for i in my_list:
@@ -13,9 +28,10 @@ for i in my_list:
     else:
         print(i)
 """
-Solution-2: Use Counter module
+Solution-3: Use Counter module
 Time Complexity: O(n), Space Complexity : O(n)
 """
+print('********Solution - 3**********')
 from collections import Counter
 
 my_list = [5, 4, 1, 2, 3, 5]
@@ -25,20 +41,30 @@ for k in c:
         print(k)
 
 """
-Solution-3: Using Set 
-Time Complexity: O(1), Space Complexity : O(1)
+Solution-4: Using Set 
+Time Complexity: O(n), Space Complexity : O(n)
 Constraints: 
 1. It only checks if the duplicate elements exist.  
 """
+print('*******Solution - 4********')
 my_list = [5, 4, 1, 2, 3, 5]
 my_set = set(my_list)
+
 if len(my_set) < len(my_list):
-    print('Duplicate Exists')
+    pass
+else:
+    print('No Duplicate')
+
+for i in my_set:
+    count = my_list.count(i)
+    if count > 1:
+        print(f'{ i } is repeated { count } times')
 
 """
-Solution-4 : Using Sorting 
-Time Complexity : O(nlogn) , Space Complexity: O(1)
+Solution-5 : Using Sorting 
+Time Complexity : O(nLogN) , Space Complexity: O(1)
 """
+print('********Solution - 5**********')
 my_list = [5, 4, 1, 2, 3, 5]
 my_list.sort()
 prev = my_list[0]
@@ -50,7 +76,7 @@ for i in my_list[1:]:
 """
 Find the duplicate number in the array using O(n) time complexity and O(1) space complexity.
 
-Solution - 5
+Solution - 6
 Ex-
 a  = [5, 4, 1, 2, 3, 5]
 Step-1: Check a[a[0]] -> a[5] and if its > 0, a[a[0]] = - a[a[0]].
@@ -68,9 +94,24 @@ Constraints:
 
 
 """
+print('********Solution - 6*********')
 my_list = [5, 4, 1, 2, 3, 5]
 for i in range(len(my_list)):
     if my_list[abs(my_list[i])] < 0:
         print(f'{abs(my_list[i])}')
     else:
         my_list[abs(my_list[i])] = - my_list[abs(my_list[i])]
+
+"""
+Solution-7 : Traverse through list , and save sum of reminders*length of list so that we can later 
+divide by len(list) to get the number of repetition. If repetitions > 1 , duplicate exists.     
+"""
+print('********Solution - 7********')
+my_list = [5, 4, 1, 2, 3, 5]
+for i in range(len(my_list)):
+    my_list[my_list[i] % len(my_list)] += len(my_list)
+
+for i in range(len(my_list)):
+    freq = my_list[i] // len(my_list)
+    if freq > 1:
+        print(f'Duplicate Elements At Index: { i } exists:{ freq } times')
