dfe0dbf5204f04470aa1f31db65d33f33836fae7
Indu Sharma <indu.sharma.acharya@gmail.com>
2021-04-27 18:32:26 +0530

    Update conftest.py

--- project_pytest/tests/conftest.py
+++ project_pytest/tests/conftest.py
@@ -18,11 +18,3 @@ def get_env_cli(request):
 @pytest.fixture(scope='session')
 def app_config(get_env_cli):
     return Config(get_env_cli).my_env
-
-
-# def pytest_configure(config):
-#     config.addinivalue_line("markers", "Mark test as testbed as applicable")
-#
-#
-# def pytest_load_initial_conftests(args):
-#     args[:] = ["-m", get_env_cli] + args
